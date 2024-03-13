b83b648320cc736d5ab3fc3cf9b1928b1abc0bd4
kun99 <kuns.km@gmail.com>
2024-03-13 07:14:56 +0700

    Working code using selenium

--- main.py
+++ main.py
@@ -1,26 +1,32 @@
-import requests
 from bs4 import BeautifulSoup
 from datetime import datetime
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
 
 x = datetime.now()
 x = x.strftime("%d/%m/%y %A")
 
 def take_in_log():
     val = input("Link: ")
-    r = requests.get(val)
-    soup = BeautifulSoup(r.content, 'html5lib')
-    name = ""
-    div_element = soup.find('a', attrs ={'class':'no-underline hover:text-blue-s dark:hover:text-dark-blue-s truncate cursor-text whitespace-normal hover:!text-[inherit]'})
-    if div_element:
-        name = div_element.text.strip()
-    difficulty = ""
-    if soup.find('div', attrs ={'class':'relative inline-flex items-center justify-center text-caption px-2 py-1 gap-1 rounded-full bg-fill-secondary text-difficulty-easy dark:text-difficulty-easy'}):
+    
+    driver = webdriver.Chrome()
+    driver.get(val)
+    name = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.CLASS_NAME, 'no-underline.hover\\:text-blue-s.dark\\:hover\\:text-dark-blue-s.truncate.cursor-text.whitespace-normal.hover\\:\\!text-\\[inherit\\]'))
+    )
+    try:
+        driver.find_element(By.CLASS_NAME, 'relative.inline-flex.items-center.justify-center.text-caption.px-2.py-1.gap-1.rounded-full.bg-fill-secondary.text-difficulty-easy.dark\:text-difficulty-easy')
         difficulty = "Easy"
-    elif soup.find('div', attrs ={'class':'relative inline-flex items-center justify-center text-caption px-2 py-1 gap-1 rounded-full bg-fill-secondary text-difficulty-medium dark:text-difficulty-medium'}):
-        difficulty = "Medium"
-    elif soup.find('div', attrs = {'class':'relative inline-flex items-center justify-center text-caption px-2 py-1 gap-1 rounded-full bg-fill-secondary text-difficulty-hard dark:text-difficulty-hard'}):
-        difficulty = "Hard"
-    print(f"{x}\n{name} - {difficulty}")
+    except NoSuchElementException:
+        try:
+            driver.find_element(By.CLASS_NAME, 'relative.inline-flex.items-center.justify-center.text-caption.px-2.py-1.gap-1.rounded-full.bg-fill-secondary.text-difficulty-medium.dark\:text-difficulty-medium')
+            difficulty = "Medium"
+        except NoSuchElementException:
+            driver.find_element(By.CLASS_NAME, 'relative.inline-flex.items-center.justify-center.text-caption.px-2.py-1.gap-1.rounded-full.bg-fill-secondary.text-difficulty-hard.dark\:text-difficulty-hard')
+            difficulty = "Hard"
+    print(f"{x}\n{name.text} - {difficulty}")
+    driver.quit()
 take_in_log()
-
-
