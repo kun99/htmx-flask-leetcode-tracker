a70994577a54b48f212f55e209255f382a0baea1
kun99 <kuns.km@gmail.com>
2024-03-14 01:49:34 +0700

    infinite scrolling and refresh on new problem submitted

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, Response
 from main import take_in_log
 from db import init_db, get_problems, add_new
 
@@ -13,8 +13,10 @@ def new_problem():
     url = request.form['problem_url']
     data = take_in_log(url)
     add_new(data)
-    problems = get_problems()
-    return render_template('table.html', data=problems)
+    
+    response = Response(status=204)
+    response.headers['HX-Refresh'] = 'true'
+    return response
     
 @app.route('/problems/', methods=['GET'])
 def problems():
@@ -23,11 +25,10 @@ def problems():
 
     start_index = (page - 1) * per_page
     end_index = start_index + per_page
-
     problems = get_problems()
     paginated_problems = problems[start_index:end_index]
-    print(paginated_problems)
-    return render_template('table.html', data=paginated_problems)
+    
+    return render_template('table.html', data=paginated_problems, page=page)
 
 if __name__ == '__main__':
     init_db()
